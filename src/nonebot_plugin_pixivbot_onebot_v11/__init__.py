aec12b6265a6c22035eec018e6c9a17c5fcf5631
ssttkkl <huang.wen.long@hotmail.com>
2022-07-17 19:13:39 +0800

    更新模块__init__.py

--- src/nonebot_plugin_pixivbot_onebot_v11/__init__.py
+++ src/nonebot_plugin_pixivbot_onebot_v11/__init__.py
@@ -1,6 +1,14 @@
+# ========= load nonebot_plugin_pixivbot =========
+import nonebot
+
+nonebot.load_plugin("nonebot_plugin_pixivbot")
+
+# ================= provide beans =================
 from nonebot_plugin_pixivbot import context
 
-from .command import *
 from .providers import *
 
 provide(context)
+
+# ================ register command ================
+from .command import *
--- src/nonebot_plugin_pixivbot_onebot_v11/providers.py
+++ src/nonebot_plugin_pixivbot_onebot_v11/providers.py
@@ -7,3 +7,6 @@ from .protocol_dep import providers as protocol_dep_providers
 def provide(context: Context):
     postman_providers.provide(context)
     protocol_dep_providers.provide(context)
+
+
+__all__ = ("provide",)
