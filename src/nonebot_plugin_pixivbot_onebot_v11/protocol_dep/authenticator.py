c4e0696ed2df43c3c9f47d18012a3be38dd05626
ssttkkl <huang.wen.long@hotmail.com>
2022-07-28 22:41:19 +0800

    修复Authenticator.group_admin

--- src/nonebot_plugin_pixivbot_onebot_v11/protocol_dep/authenticator.py
+++ src/nonebot_plugin_pixivbot_onebot_v11/protocol_dep/authenticator.py
@@ -1,5 +1,3 @@
-from nonebot import get_bot
-from nonebot.adapters.onebot.v11 import Bot
 from nonebot_plugin_pixivbot import context
 from nonebot_plugin_pixivbot.protocol_dep.authenticator import Authenticator as BaseAuthenticator, \
     AuthenticatorManager
@@ -14,6 +12,5 @@ class Authenticator(BaseAuthenticator):
         return "onebot"
 
     async def group_admin(self, post_dest: PostDestination) -> bool:
-        bot: Bot = get_bot()
-        result = await bot.get_group_member_info(group_id=post_dest.group_id, user_id=post_dest.user_id)
+        result = await post_dest.bot.get_group_member_info(group_id=post_dest.group_id, user_id=post_dest.user_id)
         return result["role"] == "owner" or result["role"] == "admin"
